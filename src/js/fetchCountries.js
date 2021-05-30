3440765ce4af4efce45660ede7cbd3954e2f4c84
serophiom <serophiom@gmail.com>
2021-05-30 21:02:39 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 import { error } from '@pnotify/core';
-// import '@pnotify/core/dist/Pnotify.css';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 function fetchCountries (name) {
