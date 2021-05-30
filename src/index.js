78b429c2f50924607f8939ab9135b08fb3ae795b
serophiom <serophiom@gmail.com>
2021-05-30 19:38:25 +0300

    файлы src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './sass/main.scss';
+import API from '../src/js/fetchCountries.js';
+import countryCardTpl from '../src/templates/country-card.hbs';
+import countriesListCardTpl from '../src/templates/countries-card.hbs';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/Pnotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const debounce = require('lodash.debounce');
+const refs = {
+   inputRef: document.querySelector('.input'),
+   cardRef: document.querySelector('.card'),
+}
+
+refs.inputRef.addEventListener('input', debounce(onInputSubmit, 400));
+
+function onInputSubmit(event) {
+    event.preventDefault();
+    const inputRefValue = refs.inputRef.value.trim();
+    console.log(inputRefValue.trim());
+    API.fetchCountries(inputRefValue).then(markUpSearchResult);
+}
+    function markUpSearchResult(country) {
+        refs.cardRef.innerHTML = '';
+        
+        if (country.length === 1) {
+            const markUp = countryCardTpl(country);
+            refs.cardRef.innerHTML = markUp;
+        } else
+
+            if (country.length >= 2 && country.length <= 10) {
+                const markUpCountriesList = countriesListCardTpl(country);
+                refs.cardRef.innerHTML = markUpCountriesList;
+                return;
+            } else
+        
+                if (country.length > 10) {
+                    error({
+                        text: 'Too many matches found. Please enter a more specific query!',
+                        delay: 3000,
+                    });
+                }
+        return;
+    }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,23 @@
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/Pnotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+function fetchCountries (name) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(country => {
+        if (!country.ok) {
+      country.status === 404
+        ? error({
+            text: 'Sorry, we can`t find entered name of country. Please try to change inputed name.',
+            delay: 3500,
+          })
+        : error({
+            text: 'Sorry, it can be issues with server.',
+            delay: 3500,
+          });
+    }
+   
+        return country.json();
+    });
+}
+
+export default {fetchCountries};
