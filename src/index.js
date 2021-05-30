dc766f09054f4113a0e120952d94c63a58a04659
serophiom <serophiom@gmail.com>
2021-05-30 21:00:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import API from '../src/js/fetchCountries.js';
 import countryCardTpl from '../src/templates/country-card.hbs';
 import countriesListCardTpl from '../src/templates/countries-card.hbs';
 import { error } from '@pnotify/core';
-// import '@pnotify/core/dist/Pnotify.css';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const debounce = require('lodash.debounce');
