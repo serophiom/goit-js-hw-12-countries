9face4ae93595f6d39e9c14304d1b2363ecc5d7c
serophiom <serophiom@gmail.com>
2021-05-30 20:22:35 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,11 @@
-import { error } from '@pnotify/core';
-import '@pnotify/core/dist/Pnotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
+// import { error } from '@pnotify/core';
+// import '@pnotify/core/dist/Pnotify.css';
+// import '@pnotify/core/dist/BrightTheme.css';
 
 function fetchCountries (name) {
     return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(country => {
         if (!country.ok) {
-      country.status === 404
+        country.status === 404
         ? error({
             text: 'Sorry, we can`t find entered name of country. Please try to change inputed name.',
             delay: 3500,
