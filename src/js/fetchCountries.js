2329e83f5d7424d07fabe4a877fcf3699512f240
serophiom <serophiom@gmail.com>
2021-06-01 09:15:20 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,15 +3,15 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 function fetchCountries (name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(country => {
-        if (!country.ok) {
-        country.status === 404
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
+        if (!response.ok) {
+        response.status === 404
         error({
             text: 'Can`t find country with this name. Please try another one.',
             delay: 3000,
           })
     }
-        return country.json();
+        return response.json();
     });
 }
 
